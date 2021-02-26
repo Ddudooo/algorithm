b19c9586086f2a10014803650c900c1bf346bfff
Ddudooo <igoa.work@gmail.com>
2021-02-26 19:10:20 +0900

    그리디 알고리즘 풀이 추가

---
+++ baekjoon13305/src/main/java/study/baekjoon13305/Application.java
@@ -0,0 +1,56 @@
+package study.baekjoon13305;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Application {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int n;
+    static long[] loads;
+    static long[] cost;
+    static long total;
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        loads = new long[n - 1];
+        cost = new long[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            loads[i] = Long.parseLong(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            cost[i] = Long.parseLong(st.nextToken());
+        }
+
+        long total = solve();
+        bw.write(String.valueOf(total));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static long solve() {
+        long before = cost[0];
+        for (int i = 1; i < n; i++) {
+            if (before <= cost[i]) {
+                cost[i] = before;
+            } else {
+                before = cost[i];
+            }
+        }
+
+        long answer = 0;
+        for (int i = 0; i < n - 1; i++) {
+            answer += cost[i] * loads[i];
+        }
+        return answer;
+    }
+}
