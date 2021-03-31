738c05cc9f535685b6472cda5a35e7db7c9bcfdd
Ddudooo <igoa.work@gmail.com>
2021-03-31 22:30:56 +0900

    프로그래머스 그래디 - 큰 수 만들기 풀이 추가

--- programmers30-42883/src/main/java/study/programmers30_42883/Solution.java
+++ programmers30-42883/src/main/java/study/programmers30_42883/Solution.java
@@ -3,8 +3,19 @@ package study.programmers30_42883;
 public class Solution {
 
 	public String solution(String number, int k) {
-		String answer = "";
-		
-		return answer;
+		StringBuilder sb = new StringBuilder();
+		int idx = 0;
+		for (int i = 0; i < number.length() - k; i++) {
+			int max = 0;
+			for (int j = idx; j <= i + k; j++) {
+				int charInt = number.charAt(j) - '0';
+				if (max < charInt) {
+					max = charInt;
+					idx = j + 1;
+				}
+			}
+			sb.append(max);
+		}
+		return sb.toString();
 	}
 }
--- programmers30-42883/src/test/java/study/programmers30_42883/SolutionTest.java
+++ programmers30-42883/src/test/java/study/programmers30_42883/SolutionTest.java
@@ -8,7 +8,17 @@ import org.junit.jupiter.params.provider.CsvSource;
 class SolutionTest {
 
 	@ParameterizedTest
-	@CsvSource({"1924,2,94", "1231234,3,3234", "4177252841,4,775841"})
+	@CsvSource({
+		"1924,2,94",
+		"1231234,3,3234",
+		"4177252841,4,775841",
+		"99991,3,99",
+		"111119,3,119",
+		"7777777,2,77777",
+		"10000,2,100",
+		"87654321,3,87654",
+		"01010,3,11"
+	})
 	void solution(String number, int k, String expected) {
 		Solution solution = new Solution();
 		String answer = solution.solution(number, k);
