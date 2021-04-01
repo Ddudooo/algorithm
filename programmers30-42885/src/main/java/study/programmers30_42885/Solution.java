0494c66b9146c04b6d156c705bfbfe36444b7f86
Ddudooo <igoa.work@gmail.com>
2021-04-01 22:21:57 +0900

    프로그래머스 그리디 - 구명보트 풀이 추가

--- programmers30-42885/src/main/java/study/programmers30_42885/Solution.java
+++ programmers30-42885/src/main/java/study/programmers30_42885/Solution.java
@@ -1,9 +1,22 @@
 package study.programmers30_42885;
 
+import java.util.Arrays;
+
 public class Solution {
 
 	public int solution(int[] people, int limit) {
 		int answer = 0;
+		Arrays.sort(people);
+		int peopleLen = people.length - 1;
+		int min = 0;
+		for (int i = peopleLen; i >= min; i--) {
+			//최대 무게 인원 기준
+			if (people[min] + people[i] <= limit) {
+				//못태운 인원중 최대 무게 인원과 최소 크기 인원
+				min++;
+			}
+			answer++;
+		}
 		return answer;
 	}
 }
