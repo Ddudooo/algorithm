3ac157782089526debcd3f4223831e5e2f691138
Ddudooo <igoa.work@gmail.com>
2021-07-28 20:02:28 +0900

    프로그래머스 - 베스트 앨범 문제 풀이 추가

--- programmers/level3/programmers_42579/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_42579/src/main/java/study/problem/Solution.java
@@ -1,9 +1,92 @@
 package study.problem;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringJoiner;
+
 public class Solution {
 
 	public int[] solution(String[] genres, int[] plays) {
-		int[] answer = {};
-		return answer;
+		int songCnt = genres.length;
+		Map<String, Integer> genreCounter = new HashMap<>();
+		List<Song> songs = new ArrayList<>();
+		for (int i = 0; i < songCnt; i++) {
+			int idx = i;
+			String genre = genres[i];
+			int play = plays[i];
+			songs.add(new Song(idx, genre, play));
+			genreCounter.put(genre, genreCounter.getOrDefault(genre, 0) + play);
+		}
+		List<Integer> answer = new ArrayList<>();
+		while (genreCounter.keySet().size() != 0) {
+			String mostPlayGenre = nextMostPlayGenre(genreCounter);
+			songs.stream()
+				.filter(song -> song.getGenre().equals(mostPlayGenre))
+				.sorted((a, b) -> {
+					if (a.getPlay() == b.getPlay()) {
+						return Integer.compare(a.getIdx(), b.getIdx());
+					}
+					return -1 * Integer.compare(a.getPlay(), b.getPlay());
+				})
+				.limit(2)
+				.forEach(song -> {
+					answer.add(song.getIdx());
+				});
+		}
+		return answer.stream().mapToInt(Integer::intValue).toArray();
+	}
+
+	private String nextMostPlayGenre(Map<String, Integer> map) {
+		Set<String> strings = map.keySet();
+		if (strings.size() == 0) {
+			return "";
+		}
+		int max = Integer.MIN_VALUE;
+		String mostPlayedGenre = "";
+		for (String genre : strings) {
+			if (max < map.get(genre)) {
+				mostPlayedGenre = genre;
+				max = map.get(genre);
+			}
+		}
+		map.remove(mostPlayedGenre);
+		return mostPlayedGenre;
+	}
+}
+
+class Song {
+
+	private final int idx;
+	private final String genre;
+	private final int play;
+
+	public Song(int idx, String genre, int play) {
+		this.idx = idx;
+		this.genre = genre;
+		this.play = play;
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public int getPlay() {
+		return play;
+	}
+
+	@Override
+	public String toString() {
+		return new StringJoiner(", ", Song.class.getSimpleName() + "[", "]")
+			.add("idx=" + idx)
+			.add("genre='" + genre + "'")
+			.add("play=" + play)
+			.toString();
 	}
 }
--- programmers/level3/programmers_42579/src/test/java/study/problem/SolutionTest.java
+++ programmers/level3/programmers_42579/src/test/java/study/problem/SolutionTest.java
@@ -18,7 +18,9 @@ class SolutionTest {
 
 	@ParameterizedTest
 	@CsvSource(value = {
-		"[\"classic\", \"pop\", \"classic\", \"classic\", \"pop\"]\t[500, 600, 150, 800, 2500]\t[4, 1, 3, 0]"
+		"[\"classic\", \"pop\", \"classic\", \"classic\", \"pop\"]\t[500, 600, 150, 800, 2500]\t[4, 1, 3, 0]",
+		"[\"A\", \"A\", \"B\", \"A\", \"B\", \"B\", \"A\", \"A\", \"A\", \"A\"]\t[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]\t[0, 1, 2, 4]",
+		"[\"classic\", \"pop\", \"classic\", \"classic\",\"jazz\",\"pop\", \"Rock\", \"jazz\"]\t[500, 600, 150, 800, 1100, 2500, 100, 1000]\t[5, 1, 4, 7, 3, 0, 6]"
 	}, delimiter = '\t')
 	void testCase(String genreStr, String playStr, String returnStr) {
 		int[] answer = this.solution.solution(strToAry(genreStr), strToIntAry(playStr));
