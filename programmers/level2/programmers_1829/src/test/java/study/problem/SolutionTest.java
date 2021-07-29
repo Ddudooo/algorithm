13d8b88a47e03c67bf8f7b07c2963d001da67d88
Ddudooo <igoa.work@gmail.com>
2021-07-29 18:04:06 +0900

    프로그래머스 - 카카오 프렌즈 컬러링북 문제 초기 설정

---
+++ programmers/level2/programmers_1829/src/test/java/study/problem/SolutionTest.java
@@ -0,0 +1,54 @@
+package study.problem;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SolutionTest {
+
+	private Solution solution;
+
+	@BeforeEach
+	void init() {
+		solution = new Solution();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {
+		"6\t4\t[[1, 1, 1, 0], [1, 2, 2, 0], [1, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 3], [0, 0, 0, 3]]\t[4, 5]"
+	}, delimiter = '\t')
+	void testCase(int m, int n, String pictureStr, String answerStr) {
+		int[] answer = this.solution.solution(m, n, strToIntDoubleAry(pictureStr));
+
+		assertArrayEquals(strToIntAry(answerStr), answer, "답이 같아야 한다.");
+	}
+
+	private int[] strToIntAry(String str) {
+		String[] split = str.split(",");
+		return Arrays.stream(split)
+			.map(s -> s.replaceAll("\\D", ""))
+			.map(String::trim)
+			.mapToInt(Integer::valueOf)
+			.toArray();
+	}
+
+	private int[][] strToIntDoubleAry(String str) {
+		String[] split = str.split("(],.?\\[)");
+		split = Arrays.stream(split)
+			.map(splitStr -> splitStr.replaceAll("[]\\[]", ""))
+			.toArray(String[]::new);
+		return Arrays.stream(split)
+			.map(splitStr ->
+				Arrays.stream(splitStr.split(","))
+					.map(String::trim)
+					.mapToInt(Integer::valueOf)
+					.toArray()
+			)
+			.toArray(int[][]::new);
+	}
+
+
+}
