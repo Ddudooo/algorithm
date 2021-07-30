fb5e3ebd119415b13eb12622b6a08c036811fc5f
Ddudooo <igoa.work@gmail.com>
2021-07-30 16:31:40 +0900

    프로그래머스 - 배달 문제 풀이 추가

--- programmers/level2/programmers_12978/src/main/java/study/problem/Solution.java
+++ programmers/level2/programmers_12978/src/main/java/study/problem/Solution.java
@@ -1,13 +1,88 @@
 package study.problem;
 
+import java.util.Arrays;
+
 public class Solution {
 
+	private final int MAX_COST = 500000;
+
 	public int solution(int N, int[][] road, int K) {
-		int answer = 0;
+		int[][] map = initMap(N);
+		setRoad(map, road);
+		floydWarshall(map, N);
+		return (int) Arrays.stream(map[0])
+			.filter(v -> v <= K)
+			.count();
+	}
 
-		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-		System.out.println("Hello Java");
+	private int[][] initMap(int N) {
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i == j) {
+					map[i][j] = 0;
+				} else {
+					map[i][j] = MAX_COST;
+				}
+			}
+		}
+		return map;
+	}
 
-		return answer;
+	private void setRoad(int[][] map, int[][] road) {
+		for (int[] rows : road) {
+			int start = rows[0] - 1;
+			int end = rows[1] - 1;
+			int cost = rows[2];
+			int preCost = map[start][end];
+			int minCost = Math.min(cost, preCost);
+			map[start][end] = minCost;
+			map[end][start] = minCost;
+		}
+	}
+
+	private void floydWarshall(int[][] map, int N) {
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (i == k || j == k || i == j) {
+						continue;
+					}
+					// i 번 마을에서 j마을로 가는데
+					int directPass = map[i][j];
+					// k 번 마을을 거치고 오는 값이 더 작을때
+					int byPass = calculateByPassCost(map[i][k], map[k][j]);
+					// 이때 각 값을 비교할때 단절된 통로는 제외한다.
+					if (directPass == MAX_COST && byPass == MAX_COST) {
+						// 둘 다 마을을 연결할 수 없을 때는 계산하지 않는다
+						continue;
+					}
+					int min = Math.min(directPass, byPass);
+					map[i][j] = min;
+					map[j][i] = min;
+				}
+			}
+		}
+	}
+
+	private void print(int[][] map) {
+		for (int[] rows : map) {
+			for (int v : rows) {
+				if (v != MAX_COST) {
+					System.out.printf("%3d ", v);
+					continue;
+				}
+				System.out.print("INF ");
+			}
+			System.out.println();
+		}
+	}
+
+	private int calculateByPassCost(int ItoK, int KtoJ) {
+		if (ItoK == MAX_COST || KtoJ == MAX_COST) {
+			// 둘중 하나라도 연결될 수 없을 때
+			return MAX_COST;
+		}
+		return ItoK + KtoJ;
 	}
 }
