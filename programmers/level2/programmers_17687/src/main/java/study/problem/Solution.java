056651c6cadd9b0f5d0b85e6909ce332534e8fe8
Ddudooo <igoa.work@gmail.com>
2021-08-02 16:45:05 +0900

    프로그래머스 - n진수 게임 풀이 추가

--- programmers/level2/programmers_17687/src/main/java/study/problem/Solution.java
+++ programmers/level2/programmers_17687/src/main/java/study/problem/Solution.java
@@ -3,7 +3,36 @@ package study.problem;
 public class Solution {
 
 	public String solution(int n, int t, int m, int p) {
-		String answer = "";
-		return answer;
+		String correctStr = init(n, t, m);
+		StringBuilder answer = new StringBuilder();
+		int turn = 0;
+		while (turn < t) {
+			int index = (turn * m) + p - 1;
+			answer.append(correctStr.charAt(index));
+			turn++;
+		}
+		return answer.toString();
+	}
+
+	private String init(int n, int t, int m) {
+		StringBuilder sb = new StringBuilder("0");
+		int maxValue = t * m;
+		for (int i = 1; i < maxValue; i++) {
+			sb.append(numToNotationStr(i, n));
+		}
+		return sb.toString();
+	}
+
+	private String numToNotationStr(int number, int notation) {
+		StringBuilder sb = new StringBuilder();
+		while (number > 0) {
+			if (number % notation < 10) {
+				sb.append(number % notation);
+			} else {
+				sb.append((char) (number % notation - 10 + 'A'));
+			}
+			number /= notation;
+		}
+		return sb.reverse().toString();
 	}
 }
