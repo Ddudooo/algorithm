285ed0e360a28528fed12f5fcdecd47dcf6860ae
Ddudooo <igoa.work@gmail.com>
2021-08-03 15:03:30 +0900

    프로그래머스 - 이진 변환 반복하기 풀이 추가

--- programmers/level2/programmers_70129/src/main/java/study/problem/Solution.java
+++ programmers/level2/programmers_70129/src/main/java/study/problem/Solution.java
@@ -3,7 +3,13 @@ package study.problem;
 public class Solution {
 
 	public int[] solution(String s) {
-		int[] answer = {};
+		int[] answer = new int[2];
+		while (!s.equals("1")) {
+			answer[0]++;
+			String replace = s.replaceAll("0", "");
+			answer[1] += s.length() - replace.length();
+			s = Integer.toBinaryString(replace.length());
+		}
 		return answer;
 	}
 }
