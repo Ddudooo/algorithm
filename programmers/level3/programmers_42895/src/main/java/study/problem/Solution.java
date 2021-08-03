633b9178f1497f9d6cb89c202fe655ca14776fdc
Ddudooo <igoa.work@gmail.com>
2021-08-03 16:55:10 +0900

    프로그래머스 - N으로 표현 문제 풀이 추가

--- programmers/level3/programmers_42895/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_42895/src/main/java/study/problem/Solution.java
@@ -2,8 +2,36 @@ package study.problem;
 
 public class Solution {
 
+	int answer = -1;
+
 	public int solution(int N, int number) {
-		int answer = 0;
+		if (N == number) {
+			return 1;
+		}
+		dfs(N, number, 0, 0);
 		return answer;
+	}
+
+	private void dfs(int n, int target, int sum, int cnt) {
+		if (cnt > 8) {
+			answer = -1;
+			return;
+		}
+		if (sum == target) {
+			if (answer == -1) {
+				answer = cnt;
+			} else {
+				answer = Math.min(answer, cnt);
+			}
+			return;
+		}
+		int newNumber = n;
+		for (int i = 0; i < 8 - cnt; i++) {
+			dfs(n, target, sum + newNumber, cnt + 1 + i);
+			dfs(n, target, sum - newNumber, cnt + 1 + i);
+			dfs(n, target, sum * newNumber, cnt + 1 + i);
+			dfs(n, target, sum / newNumber, cnt + 1 + i);
+			newNumber = newNumber * 10 + n;
+		}
 	}
 }
