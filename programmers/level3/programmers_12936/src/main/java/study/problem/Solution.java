d38484bc6fa6147ded4f4dcbe880545b62f8c6ea
Ddudooo <igoa.work@gmail.com>
2021-08-05 19:14:53 +0900

    프로그래머스 - 줄 서는 방법 풀이 중간 저장
    효율성 통과 실패

--- programmers/level3/programmers_12936/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_12936/src/main/java/study/problem/Solution.java
@@ -1,9 +1,43 @@
 package study.problem;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Solution {
 
+	private Map<Integer, Long> factorialMemorized = new HashMap<>();
+
 	public int[] solution(int n, long k) {
-		int[] answer = {};
+		init();
+		int[] answer = new int[n];
+		List<Integer> ableNums = IntStream.range(1, n + 1).boxed().collect(Collectors.toList());
+		k--;
+		for (int i = 0; i < n; i++) {
+			long factorial = factorial(n - i - 1);
+			int numberByIndex = (int) (k / factorial);
+			k = (int) (k % factorial);
+			answer[i] = ableNums.get(numberByIndex);
+			ableNums.remove(numberByIndex);
+		}
 		return answer;
+	}
+
+	private void init() {
+		factorialMemorized = new HashMap<>();
+		factorialMemorized.put(0, 1L);
+		factorialMemorized.put(1, 1L);
+		factorialMemorized.put(2, 2L);
+	}
+
+	private long factorial(int number) {
+		if (factorialMemorized.containsKey(number)) {
+			return factorialMemorized.get(number);
+		}
+		long result = number * factorial(number - 1);
+		factorialMemorized.put(number, result);
+		return result;
 	}
 }
