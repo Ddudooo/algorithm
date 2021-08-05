1a33aebb0218f61a9ff5338ad91ec01320a9e032
Ddudooo <igoa.work@gmail.com>
2021-08-05 19:30:03 +0900

    프로그래머스 - 줄 서는 방법 풀이

--- programmers/level3/programmers_12936/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_12936/src/main/java/study/problem/Solution.java
@@ -1,43 +1,26 @@
 package study.problem;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Solution {
 
-	private Map<Integer, Long> factorialMemorized = new HashMap<>();
-
 	public int[] solution(int n, long k) {
-		init();
 		int[] answer = new int[n];
-		List<Integer> ableNums = IntStream.range(1, n + 1).boxed().collect(Collectors.toList());
+		List<Integer> numbers = new ArrayList<>();
+		long factorial = 1;
+		for (int i = 1; i <= n; i++) {
+			numbers.add(i);
+			factorial *= i;
+		}
 		k--;
 		for (int i = 0; i < n; i++) {
-			long factorial = factorial(n - i - 1);
-			int numberByIndex = (int) (k / factorial);
-			k = (int) (k % factorial);
-			answer[i] = ableNums.get(numberByIndex);
-			ableNums.remove(numberByIndex);
+			factorial /= n - i;
+			int index = (int) (k / factorial);
+			answer[i] = numbers.get(index);
+			numbers.remove(index);
+			k %= factorial;
 		}
 		return answer;
-	}
-
-	private void init() {
-		factorialMemorized = new HashMap<>();
-		factorialMemorized.put(0, 1L);
-		factorialMemorized.put(1, 1L);
-		factorialMemorized.put(2, 2L);
-	}
-
-	private long factorial(int number) {
-		if (factorialMemorized.containsKey(number)) {
-			return factorialMemorized.get(number);
-		}
-		long result = number * factorial(number - 1);
-		factorialMemorized.put(number, result);
-		return result;
 	}
 }
