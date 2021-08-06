3032b8fedef05722ce3b3bd08c4dd3c161631412
Ddudooo <igoa.work@gmail.com>
2021-08-06 16:37:57 +0900

    프로그래머스 - 멀리뛰기 문제 실패한 풀이
    dfs 처리시 시간 초과

--- programmers/level3/programmers_12914/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_12914/src/main/java/study/problem/Solution.java
@@ -2,8 +2,22 @@ package study.problem;
 
 public class Solution {
 
+	private long answer = 0;
+
 	public long solution(int n) {
-		long answer = 0;
+		dfs(0, n);
 		return answer;
+	}
+
+	private void dfs(int num, int target) {
+		if (num == target) {
+			answer++;
+			return;
+		}
+		if (num > target) {
+			return;
+		}
+		dfs(num + 1, target);
+		dfs(num + 2, target);
 	}
 }
