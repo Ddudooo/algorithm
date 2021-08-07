1278606e5c3d168f441707f3e4eab30e9ee6dc0e
Ddudooo <igoa.work@gmail.com>
2021-08-07 19:13:12 +0900

    프로그래머스 - 야근 지수 초기 설정

---
+++ programmers/level3/programmers_12927/src/main/java/study/problem/Solution.java
@@ -0,0 +1,9 @@
+package study.problem;
+
+public class Solution {
+
+	public long solution(int n, int[] works) {
+		long answer = 0;
+		return answer;
+	}
+}
---
+++ programmers/level3/programmers_12927/src/test/java/study/problem/SolutionTest.java
@@ -0,0 +1,39 @@
+package study.problem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SolutionTest {
+
+	private Solution solution;
+
+	@BeforeEach
+	void init() {
+		solution = new Solution();
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {
+		"[4, 3, 3]\t4\t12",
+		"[2, 1, 2]\t1\t6",
+		"[1,1]\t3\t0"
+	}, delimiter = '\t')
+	void testCase(String workStr, int n, long result) {
+		long answer = this.solution.solution(n, strToIntAry(workStr));
+
+		assertEquals(result, answer, "답이 같아야 한다.");
+	}
+
+	private int[] strToIntAry(String str) {
+		String[] split = str.split(",");
+		return Arrays.stream(split)
+			.map(s -> s.replaceAll("\\D", ""))
+			.map(String::trim)
+			.mapToInt(Integer::valueOf)
+			.toArray();
+	}
+}
