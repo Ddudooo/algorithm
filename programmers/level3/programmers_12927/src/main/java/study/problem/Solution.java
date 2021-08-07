2f7594fd8fdffe212cf7c0c57251868b107a365d
Ddudooo <igoa.work@gmail.com>
2021-08-07 19:32:50 +0900

    프로그래머스 - 야근 지수 풀이
    효율성 시간 초과 풀이...

--- programmers/level3/programmers_12927/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_12927/src/main/java/study/problem/Solution.java
@@ -1,9 +1,38 @@
 package study.problem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
 	public long solution(int n, int[] works) {
 		long answer = 0;
+		List<Integer> workList = new ArrayList<>();
+		for (int j : works) {
+			workList.add(j);
+		}
+		boolean flag = true;
+		while (flag) {
+			int max = 0;
+			int idx = 0;
+			for (int i = 0; i < workList.size(); i++) {
+				int work = workList.get(i);
+				max = Math.max(max, work);
+				if (max == work) {
+					idx = i;
+				}
+			}
+			if (max != 0) {
+				workList.set(idx, max - 1);
+				n--;
+			}
+			if (max == 0 || n == 0) {
+				flag = false;
+			}
+		}
+		for (int work : workList) {
+			answer += Math.pow(work, 2);
+		}
 		return answer;
 	}
 }
