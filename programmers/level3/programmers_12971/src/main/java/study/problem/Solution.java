9d0506dbf5aed7acea9131912e4fcfef57d1bf55
Ddudooo <igoa.work@gmail.com>
2021-08-14 21:09:01 +0900

    프로그래머스 - 스티커모으기(2) 풀이 추가

--- programmers/level3/programmers_12971/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_12971/src/main/java/study/problem/Solution.java
@@ -1,14 +1,29 @@
 package study.problem;
 
+import java.util.Arrays;
+
 public class Solution {
 
 	public int solution(int sticker[]) {
-		int answer = 0;
+		int length = sticker.length;
+		if (length <= 3) {
+			return Arrays.stream(sticker).max().getAsInt();
+		}
+		int end = length - 1;
+		int[][] sum = new int[2][length];
+		sum[0][0] = sticker[0];
+		sum[0][1] = sticker[0];
+		sum[1][0] = 0;
+		sum[1][1] = sticker[1];
 
-		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-		System.out.println("Hello Java");
-
-		return answer;
+		for (int i = 2; i < end; i++) {
+			int currentSticker = sticker[i];
+			sum[0][i] = Math.max(sum[0][i - 1], sum[0][i - 2] + currentSticker);
+			sum[1][i] = Math.max(sum[1][i - 1], sum[1][i - 2] + currentSticker);
+		}
+		sum[0][end] = Math.max(sum[0][end - 2], sum[0][end - 1]);
+		sum[1][end] = Math.max(sum[1][end - 2] + sticker[end], sum[1][end - 1]);
+		return Math.max(sum[0][end], sum[1][end]);
 	}
 
 }
