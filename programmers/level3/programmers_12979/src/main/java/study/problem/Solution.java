57720a4a60c1092d60582a2bf7d8f83a05e9e145
Ddudooo <igoa.work@gmail.com>
2021-08-16 20:46:09 +0900

    프로그래머스 - 기지국 설치 풀이 추가

--- programmers/level3/programmers_12979/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_12979/src/main/java/study/problem/Solution.java
@@ -4,10 +4,32 @@ public class Solution {
 
 	public int solution(int n, int[] stations, int w) {
 		int answer = 0;
-
-		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-		System.out.println("Hello Java");
+		int preEnd = 1;
+		int coverage = 2 * w + 1;
+		for (int station : stations) {
+			int start = Math.max(1, station - w);
+			int end = Math.min(n, station + w);
+			int interval = start - preEnd;
+			preEnd = end + 1;
+			if (interval <= 0) {
+				continue;
+			}
+			answer += calculateStation(interval, coverage);
+		}
+		if (preEnd <= n) {
+			int interval = n + 1 - preEnd;
+			answer += calculateStation(interval, coverage);
+		}
 
 		return answer;
+	}
+
+	private int calculateStation(int interval, int coverage) {
+
+		int station = interval / coverage;
+		if (interval % coverage > 0) {
+			station++;
+		}
+		return station;
 	}
 }
