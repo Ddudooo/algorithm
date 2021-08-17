31b47a0238be75bb400245d2ad68d92ba659fedd
Ddudooo <igoa.work@gmail.com>
2021-08-17 18:00:55 +0900

    프로그래머스 - 단속카메라 풀이 추가

--- programmers/level3/programmers_42884/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_42884/src/main/java/study/problem/Solution.java
@@ -1,9 +1,26 @@
 package study.problem;
 
+import java.util.Arrays;
+
 public class Solution {
 
 	public int solution(int[][] routes) {
 		int answer = 0;
+		Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+		int preEnd = Integer.MIN_VALUE;
+		for (int[] route : routes) {
+			if (answer == 0) {
+				preEnd = route[1];
+				answer++;
+				continue;
+			}
+			int start = route[0];
+			int end = route[1];
+			if (start > preEnd) {
+				preEnd = end;
+				answer++;
+			}
+		}
 		return answer;
 	}
 }
