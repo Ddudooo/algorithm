46b8ff2b4e3ef8b992c5bc518e0d90c24443f15c
Ddudooo <igoa.work@gmail.com>
2021-08-26 22:36:51 +0900

    프로그래머스 - 불량 사용자 풀이 추가

--- programmers/level3/programmers_64064/src/main/java/study/problem/Solution.java
+++ programmers/level3/programmers_64064/src/main/java/study/problem/Solution.java
@@ -1,9 +1,45 @@
 package study.problem;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 public class Solution {
 
+	private final Set<Set<String>> candidate = new HashSet<>();
+
 	public int solution(String[] user_id, String[] banned_id) {
-		int answer = 0;
-		return answer;
+		Set<String>[] bannedMatching = new Set[banned_id.length];
+		for (int i = 0; i < banned_id.length; i++) {
+			Set<String> matching = matching(banned_id[i], user_id);
+			bannedMatching[i] = matching;
+		}
+		calculate(new HashSet<>(), 0, bannedMatching);
+		return candidate.size();
+	}
+
+	private Set<String> matching(String bannedId, String... userIds) {
+		Set<String> matchedIds = new HashSet<>();
+		String matchPattern = bannedId.replaceAll("\\*", ".");
+		for (String userId : userIds) {
+			if (Pattern.matches(matchPattern, userId)) {
+				matchedIds.add(userId);
+			}
+		}
+		return matchedIds;
+	}
+
+	private void calculate(Set<String> ableBanList, int idx, Set<String>... bannedMatching) {
+		if (idx == bannedMatching.length) {
+			candidate.add(new HashSet<>(ableBanList));
+			return;
+		}
+
+		for (String id : bannedMatching[idx]) {
+			if (ableBanList.add(id)) {
+				calculate(ableBanList, idx + 1, bannedMatching);
+				ableBanList.remove(id);
+			}
+		}
 	}
 }
